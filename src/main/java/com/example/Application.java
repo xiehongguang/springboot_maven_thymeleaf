2767d565aacc997a20a8a8e7ae9a77298e0e790d
红光 谢 <xiehongguang0228@gmail.com>
2018-06-06 20:40:22 +0800

    Initial commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
